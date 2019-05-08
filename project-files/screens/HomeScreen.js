5beafaf0dcf283c966a0e718bd689de11f073299
jonsirrine <sir15004@byui.edu>
2019-05-07 23:01:49 -0700

    basic drawer nav

---
+++ project-files/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        drawerLabel: 'Home'
+    };
+
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
