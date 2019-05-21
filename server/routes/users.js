ffbde052459aaf8fc4c19bb8d2ac4dab22af315e
jjreed05 <jjreed05@gmail.com>
2019-05-20 21:39:19 -0600

    server deletion

---
+++ server/routes/users.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const ADMIN = 'admin';
+const ADMIN_PWD = 'admin123';
+const mongoClient = require("mongodb");
+
+// here is where we will handle any database call
+const uri = 'mongodb+srv://admin:admin123@gps-time-afto7.mongodb.net/test?retryWrites=true';
+
+
+router.post("/authenticate/", function(req, res, next){
+    var username = req.body.username.toString();
+    var password = req.body.password.toString();
+
+    mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
+        if(err) {
+            throw err;
+        }
+
+        const collection = client.db("usersDb").collection("userInformation");
+        collection.findOne({"username": username, password}, (error, result) => {
+            if (error) {
+                return res.status(500).send(error);
+            }
+            if(!result)
+                res.send(false);
+            else
+                res.send(result._id);
+        });
+        client.close();
+    });
+});
+
+module.exports = router;
