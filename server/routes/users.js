b032d9ab8cda0a6ced2d1d45b2d6d834cd19323e
jonsirrine <sir15004@byui.edu>
2019-06-05 00:55:01 -0700

    initial commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,61 +10,101 @@ const saltRounds = 10;
 // here is where we will handle any database call
 const uri = 'mongodb+srv://admin:admin123@gps-time-afto7.mongodb.net/test?retryWrites=true';
 
-router.post("/authenticate/", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-})
-
 router.get('/hello', function(req, res, next){
    res.send("Hello World");
 })
 
 router.post("/authenticate/", function(req, res, next){
-    console.log(req.body);
-    var username = req.body.username.toString();
-    var password = req.body.password.toString();
+   var username = req.body.username.toString();
+   var password = req.body.password.toString();
 
-    mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
-        if(err) {
-            throw err;
-        }
-        const collection = client.db("usersDb").collection("userInformation");
-        // find the entry
-        collection.findOne({"username": username}, (error, user) => {
+   mongoClient.connect(uri, { useNewUrlParser: true },function(err, client){
+         if (err) throw err;
+
+         const collection = client.db("usersDb").collection("userInformation");
+         collection.findOne({
+            $or: [
+               { "username": username },
+               { "email": username }
+            ]
+         }, (error, user) => {
             if (error) {
                 return res.status(400).send(error);
             }
             if (!user)
-                return res.send(false);
+               return res.send(false);
             if (!bcrypt.compareSync(password, user.password))
-                return res.status(400).send(false)
-            res.send(user._id);
+               return res.status(400).send(false)
+            res.send({
+               "id": user._id,
+               "username": user.username,
+               "email": user.email,
+               "company": user.company
+               "isAdmin": false,
+            });
         });
         client.close();
     });
 });
 
 router.post("/addUser", function(req, res){
-    const username = req.body.username;
-    const email = req.body.email;
-    const company = req.body.company;
+   const username = req.body.username;
+   const email = req.body.email;
+   const company = req.body.company;
+   const password = bcrypt.hashSync(req.body.password, saltRounds);
+   let   isAdmin = false;
+   let myObject = { username, password, email, company, isAdmin};
 
-    // hash the password
-    const password = bcrypt.hashSync(req.body.password, saltRounds);
+   // connect to atlas
+   mongoClient.connect(uri, { useNewUrlParser: true }, async (err, client) => {
+      if (err) throw err;
 
-    const myObject = { username, password, email, company};
+      // determine if company already exists
+      let companyExists;
+      const companyInformation = client.db("userDb").collection("companyInformation");
+      await companyInformation.findOne({ "name": company}, (error, company) => {
+            if (error) {
+                return res.status(400).send(error);
+            }
+            companyExists = !!company; // the bang! bang! should convert the company object to a boolean 
+      });
 
-    // connect to atlas
-    mongoClient.connect(uri, { useNewUrlParser: true }, function(err, client){
-        if(err) throw err;
-        const collection = client.db("usersDb").collection("userInformation");
-        // insert
-        collection.insertOne(myObject, function(error, result){
+      // some things need to happen if the company is new
+      if (!companyExists){
+
+         // first the new user should be admin
+         isAdmin = true;
+         myObject = { username, password, email, company, isAdmin };
+
+         // and add the new company to the company collection
+         let name = company;
+         let anotherObject = { name };
+         companyInformation.inserOne(anotherObject, (error, result) => {
             if (error) throw error;
-            console.log("added the user");
-            res.send(result.insertedId);
-        });
-        client.close();
+            console.log("added company: " + company);
+         })
+         
+      }
+
+      const userInformation = client.db("usersDb").collection("userInformation");
+      userInformation.findOne({
+         $or: [
+            { "username": username },
+            { "email": username }
+         ]
+      }, (error, user) => {
+         if (error) {
+             return res.status(400).send(error);
+         }
+         if (!user){
+            userInformation.insertOne(myObject, function(error, result){
+               if (error) throw error;
+               console.log("added the user");
+               res.send(result.insertedId); // can we change this to return a user?
+            });
+         }
+      });
+      client.close();
     });
 });
 
