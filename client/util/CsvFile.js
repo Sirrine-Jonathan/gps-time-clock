46048b2732288d1dd32881d456471e2e311ff5f0
jjreed05 <jjreed05@gmail.com>
2019-07-17 22:45:27 -0600

    simple header template

---
+++ client/util/CsvFile.js
@@ -0,0 +1,10 @@
+import {CSVLink} from "react-csv";
+
+const headers = [
+    { label: "First Name", key: "firstname" },
+    { label: "Last Name", key: "lastname" },
+    { label: "Total Hours", key: "totalHours"}
+];
+
+
+export default headers;
